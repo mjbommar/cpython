59a1d6b10ae96ecffc3d43fb618684b627a4014b
Michael Bommarito <michael.bommarito@gmail.com>
2026-04-18 12:44:27 -0400

    uuid: harden c fast path across OSes

--- Modules/_uuidmodule.c
+++ Modules/_uuidmodule.c
@@ -3,11 +3,15 @@
  * DCE compatible Universally Unique Identifier library.
  */
 
-#ifndef Py_BUILD_CORE_BUILTIN
-#  define Py_BUILD_CORE_MODULE 1
+// Need limited C API version 3.14 for Py_mod_gil and PyLong_From*NativeBytes.
+#include "pyconfig.h"   // Py_GIL_DISABLED
+#ifndef Py_GIL_DISABLED
+#  define Py_LIMITED_API 0x030e0000
 #endif
 
 #include "Python.h"
+#include <errno.h>
+#include <stdlib.h>
 #if defined(HAVE_UUID_H)
   // AIX, FreeBSD, libuuid with pkgconf
   #include <uuid.h>
@@ -15,15 +19,19 @@
   // libuuid without pkgconf
   #include <uuid/uuid.h>
 #endif
-#ifdef HAVE_ERRNO_H
-#  include <errno.h>
+#if defined(HAVE_FCNTL_H)
+#  include <fcntl.h>
 #endif
 #if defined(HAVE_SYS_RANDOM_H) && (defined(HAVE_GETRANDOM) || defined(HAVE_GETENTROPY))
 #  include <sys/random.h>
 #endif
+#if defined(HAVE_UNISTD_H)
+#  include <unistd.h>
+#endif
 
 #ifdef MS_WINDOWS
-#include <rpc.h>
+#  include <windows.h>
+#  include <rpc.h>
 #endif
 
 #if !defined(MS_WINDOWS) && (defined(HAVE_UUID_H) || defined(HAVE_UUID_UUID_H))
@@ -94,15 +102,156 @@ py_windows_has_stable_node(void)
 }
 #endif
 
-#if defined(HAVE_GETRANDOM)
+#if defined(MS_WINDOWS) \
+        || defined(HAVE_ARC4RANDOM_BUF) || defined(HAVE_ARC4RANDOM) \
+        || ((defined(HAVE_GETRANDOM) || defined(HAVE_GETENTROPY)) \
+            && defined(HAVE_FCNTL_H) && defined(HAVE_UNISTD_H))
+#  define PY_UUID_HAVE_FAST_RANDOM 1
+#else
+#  define PY_UUID_HAVE_FAST_RANDOM 0
+#endif
+
+#if defined(MS_WINDOWS)
+
+#  ifndef BCRYPT_USE_SYSTEM_PREFERRED_RNG
+#    define BCRYPT_USE_SYSTEM_PREFERRED_RNG 0x00000002
+#  endif
+
+typedef LONG (NTAPI *PBCryptGenRandom)(HANDLE, PUCHAR, ULONG, ULONG);
+
+static INIT_ONCE uuid_bcrypt_init_once = INIT_ONCE_STATIC_INIT;
+static PBCryptGenRandom uuid_bcryptgenrandom = NULL;
+
+static BOOL CALLBACK
+uuid_windows_init_bcrypt(PINIT_ONCE once, PVOID parameter, PVOID *context)
+{
+    HMODULE dll = LoadLibraryW(L"bcrypt.dll");
+    PBCryptGenRandom func = NULL;
+    (void)once;
+    (void)parameter;
+    (void)context;
+
+    if (dll != NULL) {
+        func = (PBCryptGenRandom)(void (*)(void))GetProcAddress(dll, "BCryptGenRandom");
+    }
+    uuid_bcryptgenrandom = func;
+    return TRUE;
+}
+
+static int
+uuid_windows_random_available(void)
+{
+    InitOnceExecuteOnce(&uuid_bcrypt_init_once, uuid_windows_init_bcrypt, NULL, NULL);
+    return uuid_bcryptgenrandom != NULL;
+}
+
 static int
 uuid_fill_random_bytes(unsigned char *buffer, Py_ssize_t size)
+{
+    NTSTATUS status;
+
+    if (!uuid_windows_random_available()) {
+        PyErr_SetString(PyExc_RuntimeError,
+                        "BCryptGenRandom is not available on this Windows build");
+        return -1;
+    }
+
+    if (size > (Py_ssize_t)ULONG_MAX) {
+        PyErr_SetString(PyExc_OverflowError, "too many random bytes requested");
+        return -1;
+    }
+
+    status = uuid_bcryptgenrandom(NULL, (PUCHAR)buffer, (ULONG)size,
+                                  BCRYPT_USE_SYSTEM_PREFERRED_RNG);
+    if (status < 0) {
+        PyErr_Format(PyExc_OSError,
+                     "BCryptGenRandom failed with NTSTATUS 0x%08lx",
+                     (unsigned long)status);
+        return -1;
+    }
+    return 0;
+}
+
+#elif defined(HAVE_ARC4RANDOM_BUF)
+
+static int
+uuid_fill_random_bytes(unsigned char *buffer, Py_ssize_t size)
+{
+    arc4random_buf(buffer, (size_t)size);
+    return 0;
+}
+
+#elif defined(HAVE_ARC4RANDOM)
+
+static int
+uuid_fill_random_bytes(unsigned char *buffer, Py_ssize_t size)
+{
+    Py_ssize_t index = 0;
+
+    while (index < size) {
+        unsigned int chunk = arc4random();
+        size_t i = 0;
+
+        for (; i < sizeof(chunk) && index < size; i++, index++) {
+            buffer[index] = (unsigned char)(chunk >> (i * 8));
+        }
+    }
+    return 0;
+}
+
+#elif PY_UUID_HAVE_FAST_RANDOM
+
+static int
+uuid_fill_random_bytes_dev_urandom(unsigned char *buffer, Py_ssize_t size)
+{
+    int flags = O_RDONLY;
+    int fd;
+
+#  ifdef O_CLOEXEC
+    flags |= O_CLOEXEC;
+#  endif
+
+    fd = open("/dev/urandom", flags);
+    if (fd < 0) {
+        PyErr_SetFromErrno(PyExc_OSError);
+        return -1;
+    }
+
+    while (size > 0) {
+        ssize_t n = read(fd, buffer, (size_t)size);
+        if (n < 0) {
+            if (errno == EINTR) {
+                continue;
+            }
+            close(fd);
+            PyErr_SetFromErrno(PyExc_OSError);
+            return -1;
+        }
+        if (n == 0) {
+            close(fd);
+            PyErr_SetString(PyExc_OSError, "short read from /dev/urandom");
+            return -1;
+        }
+        buffer += n;
+        size -= n;
+    }
+
+    close(fd);
+    return 0;
+}
+
+#  if defined(HAVE_GETRANDOM)
+static int
+uuid_fill_random_bytes_getrandom(unsigned char *buffer, Py_ssize_t size)
 {
     while (size > 0) {
         ssize_t n = getrandom(buffer, (size_t)size, 0);
         if (n < 0) {
             if (errno == EINTR) {
                 continue;
+            }
+            if (errno == ENOSYS) {
+                return 1;
             }
             PyErr_SetFromErrno(PyExc_OSError);
             return -1;
@@ -112,15 +261,20 @@ uuid_fill_random_bytes(unsigned char *buffer, Py_ssize_t size)
     }
     return 0;
 }
-#elif defined(HAVE_GETENTROPY)
+#  endif
+
+#  if defined(HAVE_GETENTROPY)
 static int
-uuid_fill_random_bytes(unsigned char *buffer, Py_ssize_t size)
+uuid_fill_random_bytes_getentropy(unsigned char *buffer, Py_ssize_t size)
 {
     while (size > 0) {
         size_t len = (size_t)Py_MIN(size, 256);
         if (getentropy(buffer, len) < 0) {
             if (errno == EINTR) {
                 continue;
+            }
+            if (errno == ENOSYS) {
+                return 1;
             }
             PyErr_SetFromErrno(PyExc_OSError);
             return -1;
@@ -130,9 +284,31 @@ uuid_fill_random_bytes(unsigned char *buffer, Py_ssize_t size)
     }
     return 0;
 }
+#  endif
+
+static int
+uuid_fill_random_bytes(unsigned char *buffer, Py_ssize_t size)
+{
+#  if defined(HAVE_GETRANDOM)
+    int status = uuid_fill_random_bytes_getrandom(buffer, size);
+    if (status <= 0) {
+        return status;
+    }
+#  endif
+#  if defined(HAVE_GETENTROPY)
+    {
+        int status = uuid_fill_random_bytes_getentropy(buffer, size);
+        if (status <= 0) {
+            return status;
+        }
+    }
+#  endif
+    return uuid_fill_random_bytes_dev_urandom(buffer, size);
+}
+
 #endif
 
-#if defined(HAVE_GETRANDOM) || defined(HAVE_GETENTROPY)
+#if PY_UUID_HAVE_FAST_RANDOM
 static PyObject *
 py_uuid_generate_random_int(PyObject *Py_UNUSED(context),
                             PyObject *Py_UNUSED(ignored))
@@ -147,7 +323,10 @@ py_uuid_generate_random_int(PyObject *Py_UNUSED(context),
     uuid[6] = (uuid[6] & 0x0f) | 0x40;
     uuid[8] = (uuid[8] & 0x3f) | 0x80;
 
-    return _PyLong_FromByteArray(uuid, sizeof(uuid), 0, 0);
+    return PyLong_FromUnsignedNativeBytes(
+        uuid, sizeof(uuid),
+        Py_ASNATIVEBYTES_BIG_ENDIAN | Py_ASNATIVEBYTES_UNSIGNED_BUFFER
+    );
 }
 #endif
 
@@ -173,6 +352,16 @@ uuid_exec(PyObject *module)
     ADD_INT("has_uuid_generate_time_safe", 0);
 #endif
 
+#if PY_UUID_HAVE_FAST_RANDOM
+#  if defined(MS_WINDOWS)
+    ADD_INT("has_uuid_generate_random", uuid_windows_random_available());
+#  else
+    ADD_INT("has_uuid_generate_random", 1);
+#  endif
+#else
+    ADD_INT("has_uuid_generate_random", 0);
+#endif
+
 #if defined(MS_WINDOWS)
     ADD_INT("has_stable_extractable_node", py_windows_has_stable_node());
 #elif defined(HAVE_UUID_GENERATE_TIME_SAFE_STABLE_MAC)
@@ -186,7 +375,7 @@ uuid_exec(PyObject *module)
 }
 
 static PyMethodDef uuid_methods[] = {
-#if defined(HAVE_GETRANDOM) || defined(HAVE_GETENTROPY)
+#if PY_UUID_HAVE_FAST_RANDOM
     {"generate_random_int", py_uuid_generate_random_int, METH_NOARGS, NULL},
 #endif
 #if defined(HAVE_UUID_UUID_H) || defined(HAVE_UUID_H)
